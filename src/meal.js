82dbe3442136d54c984ca6b23ee78ea3cd8756a0
Sooyung Kim <kimx1279@gmail.com>
2024-02-19 15:48:38 -0700

    Add meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(stuff, price, type){
+  var item = {
+    name: stuff,
+    price: price,
+    type: type
+  }
+  return item
+}
+
+function addIngredients(ing, ingredients){
+    if (!ingredients.includes(ing)){
+      ingredients.push(ing)
+    } 
+  return ingredients
+}
+
+function formatPrice(price){
+  return `$${price}`
+}
+
+function decreasePrice(price){
+  var newPrice = price * .9
+  return newPrice
+}
+
+function createRecipe(title, ing, item){
+  var recipe = {
+    title: title,
+    ingredients: ing,
+    type: item
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
